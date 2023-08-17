0adebaf95dbe6f6725773f9c0c9791c29fd93dea
Kyler <klyerpeterson23@gmail.com>
2023-08-17 12:35:54 -0600

    added all models

---
+++ models/index.js
@@ -0,0 +1,29 @@
+const User = require("./User");
+const Comment = require("./Comment");
+const BlogPost = require("./BlogPost");
+
+User.hasMany(BlogPost, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+User.hasMany(Comment, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+Comment.belongsTo(User, {
+  foreignKey: "userId",
+});
+BlogPost.belongsTo(User, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+Comment.belongsTo(BlogPost, {
+  foreignKey: "blogPostId",
+});
+
+BlogPost.hasMany(Comment, {
+  foreignKey: "blogPostId",
+  onDelete: "CASCADE",
+});
+
+module.exports = { User, Comment, BlogPost };
